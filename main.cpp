cf6a51805bd24e3e262993413816fe304bb49ef9
Jenushan Premkumar <jenushan447@gmail.com>
2024-08-20 20:43:52 -0400

    Define direction enum and ball class with private variables

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+enum Direction {STOP, LEFT, UPLEFT, DOWNLEFT, RIGHT, UPRIGHT, DOWNRIGHT};
+
+class Ball {
+    private:
+    int x, y;
+    int originalPX, originalPY;
+    Direction currentDirection;
+};
+
+int main() {
+
+
+}
